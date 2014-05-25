34f5e932e4bdd2e066ef65a04ccbf4dfe3e92816
Cedric Hurst <cedric@spantree.net>
2014-05-25 11:08:42 -0500

    updating package name to match .app file

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@
 #
 #   include istatmenus4
 class istatmenus4 {
-  package { 'iStatMenus4':
+  package { 'iStat Menus':
     provider => 'compressed_app',
     source   => 'http://s3.amazonaws.com/bjango/files/istatmenus4/istatmenus4.10.zip',
   }
